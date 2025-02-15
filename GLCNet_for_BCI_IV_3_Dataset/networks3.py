6e8ef686dc121fc02da51b90b2e23cd20265db27
yangyanling880 <yyl09282020@163.com>
2025-02-15 14:20:38 +0800

    Add files via upload

---
+++ GLCNet_for_BCI_IV_3_Dataset/networks3.py
@@ -0,0 +1,186 @@
+from scipy.signal import hilbert
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+
+
+def threshold_adjacency_matrix(adj_matrix, percentage):
+
+    adj_matrix=np.array(adj_matrix.cpu())
+
+
+    # 计算保留的元素个数
+    num_elements = adj_matrix.size
+    num_to_keep = int(np.floor(num_elements * percentage / 100))
+
+    # 展平矩阵并排序
+    flat_matrix = adj_matrix.flatten()
+    threshold_value = np.partition(flat_matrix, -num_to_keep)[-num_to_keep]
+
+    # 将小于阈值的元素置为0
+    thresholded_matrix = np.where(adj_matrix >= threshold_value, adj_matrix, 0)
+
+    tadj=torch.tensor(thresholded_matrix,device='cuda:0')
+
+    return tadj
+
+
+
+class PLILayer(nn.Module):
+    def __init__(self,threshold=0):
+        super(PLILayer, self).__init__()
+        self.threshold= threshold
+
+    @staticmethod
+    def compute_pli(x, y):
+        thetax = torch.angle(torch.tensor(hilbert(x.cpu().numpy())).to('cuda:0'))
+        thetay = torch.angle(torch.tensor(hilbert(y.cpu().numpy())).to('cuda:0'))
+        thetadiff = thetax - thetay
+        thetadiffmodify = torch.angle(torch.exp(1j * thetadiff))
+        thetadiffmodify[(thetadiffmodify == np.pi) | (thetadiffmodify == -np.pi)] = 0
+        return torch.abs(torch.mean(torch.sign(thetadiffmodify)))
+
+    def forward(self, x):
+        batch_size, channels, samples = x.shape
+        PLI = torch.zeros((batch_size, channels, channels), dtype=torch.float32)
+        for batch in range(batch_size):
+            for i in range(channels):
+                for j in range(channels):           #   这个地方ij算了两遍，时间浪费了一倍，可以减少
+                    if i != j:
+                        value=self.compute_pli(x[batch, i, :], x[batch, j, :])
+                        if value>=self.threshold:
+                            PLI[batch, i, j] = value
+                        else:
+                            PLI[batch, i, j] = 0
+        return PLI
+
+
+class PLILayer2(nn.Module):
+    def __init__(self,threshold=0):
+        super(PLILayer2, self).__init__()
+        self.threshold= threshold
+
+    @staticmethod
+    def compute_pli(x, y):
+        thetax = torch.angle(torch.tensor(hilbert(x.cpu().numpy())).to('cuda:0'))
+        thetay = torch.angle(torch.tensor(hilbert(y.cpu().numpy())).to('cuda:0'))
+        thetadiff = thetax - thetay
+        thetadiffmodify = torch.angle(torch.exp(1j * thetadiff))
+        thetadiffmodify[(thetadiffmodify == np.pi) | (thetadiffmodify == -np.pi)] = 0
+        return torch.abs(torch.mean(torch.sign(thetadiffmodify)))
+
+    def forward(self, x):
+        batch_size, channels, samples = x.shape
+        PLI = torch.zeros((batch_size, channels, channels), dtype=torch.float32)
+        for batch in range(batch_size):
+            for i in range(channels):
+                for j in range(i):           #   这个地方ij算了两遍，时间浪费了一倍，可以减少
+                    value=self.compute_pli(x[batch, i, :], x[batch, j, :])
+                    if value>=self.threshold:
+                        PLI[batch, i, j] = PLI[batch, j, i] = value
+
+        return PLI
+
+
+
+class GraphConvolution(nn.Module):
+    def __init__(self, input_dim, output_dim, use_bias=True):
+        """图卷积：H*X*\theta
+        Args:
+        ----------
+            input_dim: int
+                节点输入特征的维度
+            output_dim: int
+                输出特征维度
+            use_bias : bool, optional
+                是否使用偏置
+        """
+        super(GraphConvolution, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.use_bias = use_bias
+        self.weight = nn.Parameter(torch.Tensor(input_dim, output_dim))
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.Tensor(output_dim))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()  # 初始化w
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight)
+        # init.kaiming_uniform_神经网络权重初始化，神经网络要优化一个非常复杂的非线性模型，而且基本没有全局最优解，
+        # 初始化在其中扮演着非常重要的作用，尤其在没有BN等技术的早期，它直接影响模型能否收敛。
+
+        if self.use_bias:
+            init.zeros_(self.bias)
+
+    def forward(self, adjacency, input_feature):
+        """邻接矩阵是稀疏矩阵，因此在计算时使用稀疏矩阵乘法
+        Args:
+        -------
+            adjacency: torch.sparse.FloatTensor
+                邻接矩阵
+            input_feature: torch.Tensor
+                输入特征
+        """
+        support = torch.matmul(input_feature, self.weight)
+        output = torch.matmul(adjacency.to('cuda:0'), support.transpose(0,1)).transpose(0,1)
+        if self.use_bias:
+            output += self.bias
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+            + str(self.input_dim) + ' -> ' \
+            + str(self.output_dim) + ')'
+
+
+class GcnNet(nn.Module):
+    """
+    定义一个包含两层GraphConvolution的模型
+    """
+    def Convlayer(self,input_dim):
+        return nn.Sequential(
+            nn.Conv2d(input_dim,input_dim,(1,10)),
+            nn.BatchNorm2d(input_dim),
+            nn.ELU(),
+            nn.MaxPool2d((1,10),stride=(1,10)),
+            nn.Dropout(p=0.25)
+        )
+    def Conlayer2(self):
+        return nn.Sequential(
+            nn.Conv2d(9,288,(10,1)),
+            nn.BatchNorm2d(288),
+            nn.ELU(),
+            nn.Dropout(p=0.25)
+        )
+
+    def __init__(self, input_dim=1433):
+        super(GcnNet, self).__init__()
+        self.Conv = self.Convlayer(input_dim)
+        self.gcn1 = GraphConvolution(39, 32)
+        self.gcn2 = GraphConvolution(32, 5)
+        self.Conv2=self.Conlayer2()
+
+
+
+    def forward(self, feature,adjacency):
+        feature=self.Conv(feature)
+
+        adjacency=threshold_adjacency_matrix(adjacency,80)
+
+        hs=self.gcn1(adjacency, feature)
+        h = F.relu(hs)
+        logits = self.gcn2(adjacency, h)
+        logits=self.Conv2(logits)
+        return logits
+
+
+
+
+
---
+++ GLCNet_for_MEG-BCI_Dataset/networks.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+import sys
+current_module = sys.modules[__name__]
+import numpy as np
+from scipy.signal import hilbert
+current_module = sys.modules[__name__]
+from braindecode.models import *
+from torch_geometric.nn import GCNConv
+import itertools
+from torch.nn import LSTM
+import pickle
+from networks3 import *
+
+
+class Conv2dWithConstraint(nn.Conv2d):
+    def __init__(self, *args, doWeightNorm = True, max_norm=1, **kwargs):
+        self.max_norm = max_norm
+        self.doWeightNorm = doWeightNorm
+        super(Conv2dWithConstraint, self).__init__(*args, **kwargs)
+
+    def forward(self, x):
+        if self.doWeightNorm:
+            self.weight.data = torch.renorm(
+                self.weight.data, p=2, dim=0, maxnorm=self.max_norm
+            )
+        return super(Conv2dWithConstraint, self).forward(x)
+
+class LinearWithConstraint(nn.Linear):
+    def __init__(self, *args, doWeightNorm = True, max_norm=1, **kwargs):
+        self.max_norm = max_norm
+        self.doWeightNorm = doWeightNorm
+        super(LinearWithConstraint, self).__init__(*args, **kwargs)
+
+    def forward(self, x):
+        if self.doWeightNorm:
+            self.weight.data = torch.renorm(
+                self.weight.data, p=2, dim=0, maxnorm=self.max_norm
+            )
+        return super(LinearWithConstraint, self).forward(x)
+
+#%% Support classes for FBNet Implementation
+class VarLayer(nn.Module):
+    '''
+    The variance layer: calculates the variance of the data along given 'dim'
+    '''
+    def __init__(self, dim):
+        super(VarLayer, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return x.var(dim = self.dim, keepdim= True)
+
+class StdLayer(nn.Module):
+    '''
+    The standard deviation layer: calculates the std of the data along given 'dim'
+    '''
+    def __init__(self, dim):
+        super(StdLayer, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return x.std(dim = self.dim, keepdim=True)
+
+class LogVarLayer(nn.Module):
+    '''
+    The log variance layer: calculates the log variance of the data along given 'dim'
+    (natural logarithm)
+    '''
+    def __init__(self, dim):
+        super(LogVarLayer, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return torch.log(torch.clamp(x.var(dim = self.dim, keepdim= True), 1e-6, 1e6))
+
+class MeanLayer(nn.Module):
+    '''
+    The mean layer: calculates the mean of the data along given 'dim'
+    '''
+    def __init__(self, dim):
+        super(MeanLayer, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return x.mean(dim = self.dim, keepdim=True)
+
+class MaxLayer(nn.Module):
+    '''
+    The max layer: calculates the max of the data along given 'dim'
+    '''
+    def __init__(self, dim):
+        super(MaxLayer, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        ma ,ima = x.max(dim = self.dim, keepdim=True)
+        return ma
+
+class swish(nn.Module):
+    '''
+    The swish layer: implements the swish activation function
+    '''
+    def __init__(self):
+        super(swish, self).__init__()
+
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+
+class SquareLayer(nn.Module):
+    '''
+    The Square layer: claculates the Square of the data.
+    '''
+
+    def __init__(self):
+        super(SquareLayer, self).__init__()
+
+    def forward(self, x):
+        return x * x
+
+
+class LogLayer(nn.Module):
+    '''
+    The log layer: claculates the log of the data.
+    '''
+
+    def __init__(self):
+        super(LogLayer, self).__init__()
+
+    def forward(self, x):
+        return torch.log(torch.clamp(x, min=1e-6))
+
+
+class PLILayer(nn.Module):
+    def __init__(self):
+        super(PLILayer, self).__init__()
+
+    @staticmethod
+    def compute_pli(x, y):
+        thetax = torch.angle(torch.tensor(hilbert(x.numpy())))
+        thetay = torch.angle(torch.tensor(hilbert(y.numpy())))
+        thetadiff = thetax - thetay
+        thetadiffmodify = torch.angle(torch.exp(1j * thetadiff))
+        thetadiffmodify[(thetadiffmodify == np.pi) | (thetadiffmodify == -np.pi)] = 0
+        return torch.abs(torch.mean(torch.sign(thetadiffmodify)))
+
+    def forward(self, x):
+        batch_size, channels, samples = x.shape
+        PLI = torch.zeros((batch_size, channels, channels), dtype=torch.float32)
+        for batch in range(batch_size):
+            for i in range(channels):
+                for j in range(channels):
+                    if i != j:
+                        PLI[batch, i, j] = self.compute_pli(x[batch, i, :], x[batch, j, :])
+        return PLI
+
+
+
+
+
+
+class GLCNet(nn.Module):
+    # just a FBCSP like structure : chan conv and then variance along the time axis
+    '''
+        FBNet with seperate variance for every 1s.
+        The data input is in a form of batch x 1 x chan x time x filterBand
+    '''
+
+    def SCB(self, m, nChan, nBands, doWeightNorm=True, *args, **kwargs):
+        '''
+        The spatial convolution block
+        m : number of sptatial filters.
+        nBands: number of bands in the data
+        '''
+        return nn.Sequential(
+            Conv2dWithConstraint(nBands, m * nBands, (nChan, 1), groups=nBands,
+                                 max_norm=2, doWeightNorm=doWeightNorm, padding=0),
+            nn.BatchNorm2d(m * nBands),
+            swish()
+        )
+
+    def LastBlock(self, inF, outF, doWeightNorm=True, *args, **kwargs):
+        return nn.Sequential(
+            LinearWithConstraint(inF, outF, max_norm=0.5, doWeightNorm=doWeightNorm, *args, **kwargs),
+            nn.LogSoftmax(dim=1))
+
+
+
+    def __init__(self, nChan, nTime, nClass=2, nBands=9, m=32,
+                 temporalLayer='LogVarLayer', strideFactor=5, doWeightNorm=True, *args, **kwargs):
+        super(GLCNet, self).__init__()
+
+        self.nBands = nBands
+        self.m = m
+        self.strideFactor = strideFactor
+
+        # create all the parrallel SCBc
+        self.scb = self.SCB(m, nChan, self.nBands, doWeightNorm=doWeightNorm)
+
+        # Formulate the temporal agreegator
+        self.temporalLayer = current_module.__dict__[temporalLayer](dim=3)
+
+        # The final fully connected layer
+        self.lastLayer = self.LastBlock(self.m * self.nBands * self.strideFactor, nClass, doWeightNorm=doWeightNorm)
+        self.GCNLayer=GcnNet(9)
+        self.TimeLSTMLayer = nn.LSTM(2500,5)
+
+
+    def forward(self, x,adj):
+
+        x = torch.squeeze(x.permute((0, 4, 2, 3, 1)), dim=4)
+        x2=self.GCNLayer(x,torch.tensor(adj,device='cuda:0'))
+        x = self.scb(x)
+        x3 = torch.unsqueeze(self.TimeLSTMLayer(torch.squeeze(x,dim=2))[0],dim=-1)
+        x = x.reshape([*x.shape[0:2], self.strideFactor, int(x.shape[3] / self.strideFactor)])
+        x = self.temporalLayer(x)+x2.transpose(-1,-2) + x3
+        x = torch.flatten(x, start_dim=1)
+        x = self.lastLayer(x)
+        return x
+
+
+
+
